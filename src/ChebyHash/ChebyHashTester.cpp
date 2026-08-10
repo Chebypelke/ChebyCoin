3cb31b43b466e51411df2ae64e4a684feccd26d9
Chebypelke <andrey.phonk@gmail.com>
2026-08-10 21:48:14 +0400

    ChebyHash32: initial working implementation

--- src/ChebyHash/ChebyHashTester.cpp
+++ src/ChebyHash/ChebyHashTester.cpp
@@ -2,17 +2,29 @@
 #include <bit>
 #include <cstdint>
 #include <iostream>
+#include <chrono>
 
 void ChebyHashTester()
 {
+    auto start = std::chrono::high_resolution_clock::now();
+    
     auto total = std::uint64_t{0};
     constexpr std::uint32_t TEST_COUNT = 10'000'000;
 
+    auto attempt_start = std::chrono::high_resolution_clock::now();
+
     for (std::uint32_t y = 0; y < TEST_COUNT; ++y)
     {
-        if (y % 100000 == 0)
+        if ((y + 1) % 100'000 == 0)
         {
-            std::cout << "Attempt: " << y << '\n';
+            auto attempt_end = std::chrono::high_resolution_clock::now();
+            
+            auto attempt_duration = std::chrono::duration<double>(attempt_end - attempt_start);
+
+            std::cout << "Attempt: " << y+1 << "," 
+                      << " duration: " << attempt_duration.count() << "s." << std::endl;
+
+            attempt_start = attempt_end;
         }
 
         for(int bit = 0; bit < 32; bit++)
@@ -31,5 +43,10 @@ void ChebyHashTester()
 
     auto average = static_cast<double>(total) / (TEST_COUNT * 32);
 
-    std::cout << "Average: " << average << '\n';
+    auto end = std::chrono::high_resolution_clock::now();
+            
+    auto duration = std::chrono::duration<double>(end - start);
+
+    std::cout << "Average: " << average << std::endl 
+              << "Duration: " << duration.count() << "s." << std::endl;
 }
