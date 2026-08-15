166d0535e791cdeee6cfc62b0c5914ae7de2f59a
Chebypelke <andrey.phonk@gmail.com>
2026-08-15 22:18:07 +0400

    ChebyWallet: add wallet info

--- src/ChebyCoinApp/App.cpp
+++ src/ChebyCoinApp/App.cpp
@@ -35,26 +35,26 @@ void ChebyCoinApp::App::run()
                 {
                     clearScreen();
 
-                    std::cout << "[*] Generating wallet..." << std::endl;
+                    std::cout << "[+] Generating wallet...";
 
                     wallet = ChebyWallet::Wallet::generateWallet();
 
-                    std::cout << "[+] Generating wallet... DONE!" << std::endl << std::endl;
+                    std::cout << " DONE!" << std::endl << std::endl;
 
                     std::cout << "Wallet address: "
-                            << std::hex
-                            << wallet->address.value.high << wallet->address.value.low 
-                            << std::dec
-                            << std::endl;
+                              << std::hex
+                              << wallet->address.value.high << wallet->address.value.low 
+                              << std::dec
+                              << std::endl;
 
                     std::cout << "Wallet publickey: "
-                            << std::hex
-                            << wallet->keyPair.publicKey.publicExponent.high
-                            << wallet->keyPair.publicKey.publicExponent.low
-                            << wallet->keyPair.publicKey.modulus.high
-                            << wallet->keyPair.publicKey.modulus.low
-                            << std::dec
-                            << std::endl;
+                              << std::hex
+                              << wallet->keyPair.publicKey.publicExponent.high
+                              << wallet->keyPair.publicKey.publicExponent.low
+                              << wallet->keyPair.publicKey.modulus.high
+                              << wallet->keyPair.publicKey.modulus.low
+                              << std::dec
+                              << std::endl;
                     
                     std::cout << "To turn back press enter";
                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -82,9 +82,68 @@ void ChebyCoinApp::App::run()
 
                     break;
                 }
-                case ChebyCoinApp::WalletMenu::WalletMenuChoice::WalletInfo: // Заглушка
+                case ChebyCoinApp::WalletMenu::WalletMenuChoice::WalletInfo: 
+                {
                     clearScreen();
+
+                    std::cout << "[*] Reading wallet info...";
+                    std::cout << " DONE!" << std::endl;
+                    
+                    std::cout << "Wallet address: "
+                              << std::hex
+                              << wallet->address.value.high << wallet->address.value.low
+                              << std::dec 
+                              << std::endl;
+                    
+                    std::cout << "Wallet publickey: "
+                              << std::hex
+                              << wallet->keyPair.publicKey.publicExponent.high
+                              << wallet->keyPair.publicKey.publicExponent.low
+                              << wallet->keyPair.publicKey.modulus.high
+                              << wallet->keyPair.publicKey.modulus.low
+                              << std::dec
+                              << std::endl; 
+                              
+                    std::cout << "Wallet privatekey: "
+                              << std::hex
+                              << wallet->keyPair.privateKey.privateExponent.high
+                              << wallet->keyPair.privateKey.privateExponent.low
+                              << wallet->keyPair.privateKey.modulus.high
+                              << wallet->keyPair.privateKey.modulus.low
+                              << std::dec
+                              << std::endl;    
+
+                    std::cout << "[*] Verifying keys...";
+
+                    ChebySignature::Hash128 messageHash{
+                        5643093659353030982ULL,     // FANFORIN
+                        6076003192308203604ULL      // TURNITUP
+                    };
+
+                    ChebySignature::PrivateKey privateKey = wallet->keyPair.privateKey;
+                    ChebySignature::PublicKey publicKey = wallet->keyPair.publicKey;
+                    ChebySignature::Hash128 signature = ChebySignature::Signature::sign(messageHash, privateKey);
+                    ChebySignature::Hash128 verified = ChebySignature::Signature::verify(signature, publicKey);
+
+                    std::cout << " DONE!" << std::endl;
+                    
+                    auto expected = ChebyHash::ChebyHash128::mod(messageHash, publicKey.modulus);
+
+                    bool valid = verified.high == expected.high && verified.low == expected.low;
+
+                    std::cout << "Verified: "
+                              << (valid ? "YES" : "NO")
+                              << std::endl;
+
+                    std::cout << "To turn back press enter";
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cin.get();
+
+                    clearScreen();
+
                     break;
+                }
+
                 case ChebyCoinApp::WalletMenu::WalletMenuChoice::Balance: // Заглушка
                     clearScreen();
                     break;
