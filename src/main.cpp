15381f239b98939fc04a50236dd4223f501b5a12
Chebypelke <andrey.phonk@gmail.com>
2026-09-09 22:59:48 +0400

    ChebyCoinApp: change main.cpp start function from blockchain tester to app

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include "ChebyCoinApp/ChebyCoinApp.hpp"
-#include "ChebyChain/Testers/ChebyChainTesters.hpp"
 
 int main()
 {
-    //ChebyCoinApp::App app;
-    //app.run();
-    ChebyChain::Testers::ChebyChainTester();
+    ChebyCoinApp::App app;
+    app.run();
 }
